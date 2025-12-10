cd2ad3a3f294fd2b8a72a53eacc0b86bcc749174
Nathan Steele <nsteele1031@gmail.com>
2025-12-10 10:26:09 -0500

    Added item info panel

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, items
 
 class PersonPanel:
     def __init__(self, x, y, width, height):
@@ -22,3 +22,23 @@ class PersonPanel:
     
     def draw(self, screen):
         pygame.draw.rect(screen, (0, 0, 0), (self.x, self.y, self.width, self.height))
+
+class ItemPanel:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+    def display_item_info(self, screen, item: items.Item):
+        if item is None:
+            return
+        font = pygame.font.Font(None, 20)
+        text = font.render(f"Name: {item.name}", True, (255, 255, 255))
+        screen.blit(text, (self.x + 10, self.y + 35))
+        text = font.render(f"Description: {item.description}", True, (255, 255, 255))
+        screen.blit(text, (self.x + 10, self.y + 45))
+        text = font.render(f"Demand: {item.demand}", True, (255, 255, 255))
+        screen.blit(text, (self.x + 10, self.y + 55))
+        text = font.render(f"X: {item.x}, Y: {item.y}", True, (255, 255, 255))
+        screen.blit(text, (self.x + 10, self.y + 65))
