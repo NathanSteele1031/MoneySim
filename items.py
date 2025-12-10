7eae88cc147ae695e774125093bb14afd880657a
Nathan Steele <nsteele1031@gmail.com>
2025-12-10 10:16:44 -0500

    Added cords for items class and a method to set them

--- items.py
+++ items.py
@@ -1,15 +1,26 @@
+import pygame
+
 class Item:
     def __init__(self):
         self.name = ''
         self.description = ''
         self.demand = 0
         self.need_satisfied = []
+        self.x = 0
+        self.y = 0
     
     def add_demand(self, amount):
         self.demand += amount
     
     def remove_demand(self, amount):
         self.demand -= amount
+    
+    def set_cord(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 5)
 
 class Weapon(Item):
     def __init__(self):
