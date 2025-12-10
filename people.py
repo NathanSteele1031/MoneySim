533b6db0a65d569c63298621b885594b35f2e717
Nathan Steele <nsteele1031@gmail.com>
2025-12-09 21:19:53 -0500

    Added people class and methods

---
+++ people.py
@@ -0,0 +1,13 @@
+import pygame, random
+
+class Person():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def random_move(self):
+        self.x += random.randint(-1, 1) * 10
+        self.y += random.randint(-1, 1) * 10
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0, 0, 0), (self.x, self.y), 5)
