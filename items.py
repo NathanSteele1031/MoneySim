1c8c419ed57b205d1058045dd28dfe121f4b7fdc
Nathan Steele <nsteele1031@gmail.com>
2025-12-14 09:04:26 -0500

    Added item subclass and a function to load json items

--- items.py
+++ items.py
@@ -1,13 +1,20 @@
-import pygame
+import pygame, json
+
+def load_json_items(path):
+    try:
+        with open(path, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
 
 class Item:
-    def __init__(self):
-        self.name = ""
-        self.description = ""
-        self.demand = 0
-        self.need_satisfied = []
-        self.x = 0
-        self.y = 0
+    def __init__(self, name, description="", demand=0, need_satisfied=[], x=0, y=0):
+        self.name = name
+        self.description = description
+        self.demand = demand
+        self.need_satisfied = need_satisfied
+        self.x = x
+        self.y = y
     
     def add_demand(self, amount):
         self.demand += amount
@@ -23,9 +30,13 @@ class Item:
         pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 5)
 
 class Weapon(Item):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name, description="", demand=0, need_satisfied=[], x=0, y=0):
+        super().__init__(name, description, demand, need_satisfied, x, y)
     
 class Jewelry(Item):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name, description="", demand=0, need_satisfied=[], x=0, y=0):
+        super().__init__(name, description, demand, need_satisfied, x, y)
+
+class Consumable(Item):
+    def __init__(self, name, description="", demand=0, need_satisfied=[], x=0, y=0):
+        super().__init__(name, description, demand, need_satisfied, x, y)
