40f71d481d0d47ea6a914ac4bb6dfda8cceb45e1
Nathan Steele <nsteele1031@gmail.com>
2025-12-14 10:02:15 -0500

    Made it more terminal friendly

--- people.py
+++ people.py
@@ -1,4 +1,4 @@
-import pygame, random, json
+import pygame, random, json, curses
 import inventory
 
 class Person():
@@ -14,9 +14,17 @@ class Person():
             needs = json.load(f)
         return random.choice(needs["item_properities"])
     
-    def random_move(self):
-        self.x += random.randint(-1, 1) * 10
-        self.y += random.randint(-1, 1) * 10
+    def random_move(self, terminal_view=False):
+        if terminal_view:
+            self.x += random.randint(-1, 1)
+            self.y += random.randint(-1, 1)
+        else:
+            self.x += random.randint(-1, 1) * 10
+            self.y += random.randint(-1, 1) * 10
     
     def draw(self, screen):
         pygame.draw.circle(screen, (0, 0, 0), (self.x, self.y), 5)
+    
+    def draw_terminal(self, screen):
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        screen.addch(self.y, self.x, "P", curses.color_pair(1))
